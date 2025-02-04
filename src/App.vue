7034fb8952ec5177b517339ee98aa6c11b0d4b54
Woody Quintanilha <woodyflq@gmail.com>
2025-02-04 03:08:21 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import LedControl from './components/ledControl.vue';
+
+// Substitua pela URL correta da sua Lambda Function
+const LAMBDA_URL = "https://boj3vfxwwdnwduzz7fsm2qoo5a0tmpoy.lambda-url.sa-east-1.on.aws/";
+
+</script>
+
+<template>
+  <div>
+    <LedControl />
+  </div>
+</template>
---
+++ src/components/ledControl.vue
@@ -0,0 +1,39 @@
+<script setup>
+import axios from "axios";
+
+const lambdaUrl = "https://boj3vfxwwdnwduzz7fsm2qoo5a0tmpoy.lambda-url.sa-east-1.on.aws/";
+
+const sendCommand = async (command) => {
+  try {
+    const response = await axios.post(lambdaUrl, { command });
+    console.log("Resposta da Lambda:", response.data);
+    alert(response.data.message || "Comando enviado com sucesso!");
+  } catch (error) {
+    console.error("Erro ao chamar a Lambda:", error);
+    alert("Erro ao enviar o comando.");
+  }
+};
+</script>
+
+<template>
+  <div>
+    <button @click="sendCommand('TurnOnIntent')">Ligar Luz</button>
+    <button @click="sendCommand('TurnOffIntent')">Desligar Luz</button>
+  </div>
+</template>
+
+<style scoped>
+button {
+  padding: 10px 20px;
+  margin: 10px;
+  font-size: 16px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+}
+button:hover {
+  background-color: #45a049;
+}
+</style>
